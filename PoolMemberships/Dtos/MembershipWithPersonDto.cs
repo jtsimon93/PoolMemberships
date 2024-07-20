d83c3cd5d1f306a878bb31d56e09b0f0aa80378f
Justin Simon <jtsimon@jtsimon.dev>
2024-07-19 20:30:39 -0400

    Create Membership with Person DTO

---
+++ PoolMemberships/Dtos/MembershipWithPersonDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PoolMemberships.Dtos;
+
+public class MembershipWithPersonDto
+{
+    public int MembershipId { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public bool Active { get; set; }
+    public string PersonFirstName { get; set; }
+    public string PersonLastName { get; set; }
+    public string? PersonEmail { get; set; }
+    public string? PersonPhoneNumber { get; set; }
+    public string? PersonAddress { get; set; }
+    public string? PersonCity { get; set; }
+    public string? PersonState { get; set; }
+    public string? PersonZip { get; set; }
+}
