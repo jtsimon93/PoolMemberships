4cf1b17933e8a0ed6a40be47afa550cb2c93b482
Justin Simon <jtsimon@jtsimon.dev>
2024-07-19 20:34:24 -0400

    Update MappingProfile.cs

--- PoolMemberships/Profiles/MappingProfile.cs
+++ PoolMemberships/Profiles/MappingProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace PoolMemberships.Profiles;
+﻿using AutoMapper;
+using PoolMemberships.Dtos;
+using PoolMemberships.Models;
 
-public class MappingProfile
+namespace PoolMemberships.Profiles;
+
+public class MappingProfile : Profile
 {
-    
+    public MappingProfile()
+    {
+        CreateMap<Membership, MembershipWithPersonDto>()
+            .ForMember(dest => dest.PersonFirstName, opt => opt.MapFrom(src => src.Person.FirstName))
+            .ForMember(dest => dest.PersonLastName, opt => opt.MapFrom(src => src.Person.LastName));
+    }
 }
