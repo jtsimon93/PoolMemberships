f3236a3b5d7c25c3bf9636862488aaf4013024b0
Justin Simon <jtsimon@jtsimon.dev>
2024-07-27 12:14:00 -0400

    Add return to list button

--- PoolMemberships/ViewModels/ViewMembershipViewModel.cs
+++ PoolMemberships/ViewModels/ViewMembershipViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 using PoolMemberships.Services;
+using PoolMemberships.Views;
 
 namespace PoolMemberships.ViewModels;
 
@@ -30,10 +34,13 @@ public partial class ViewMembershipViewModel : ViewModelBase
     private string _keyFobId;
     
     private readonly IMembershipService _membershipService;
+    
+    public ICommand ReturnToListCommand { get; }
 
     public ViewMembershipViewModel(IMembershipService membershipService)
     {
         _membershipService = membershipService;
+        ReturnToListCommand = new RelayCommand(OnReturnToList);
     }
 
     public async void PopulateData(int membershipId)
@@ -54,7 +61,24 @@ public partial class ViewMembershipViewModel : ViewModelBase
         State = membership.PersonState == null ? String.Empty : membership.PersonState;
         ZipCode = membership.PersonZip == null ? String.Empty : membership.PersonZip;
         KeyFobId = membership.KeyFobId == null ? String.Empty : membership.KeyFobId;
+        
+        // membership.StartDate and membership.EndDate are DateOnly
+        // StartDate and EndDate are DateTimeOffset, so we need to convert them
+        StartDate = new DateTimeOffset(membership.StartDate.Year, membership.StartDate.Month, membership.StartDate.Day, 0, 0, 0, TimeSpan.Zero);
+        EndDate = new DateTimeOffset(membership.EndDate.Year, membership.EndDate.Month, membership.EndDate.Day, 0, 0, 0, TimeSpan.Zero);
 
+    }
+
+    private void OnReturnToList()
+    {
+       var mainWindowViewModel = App.Services.GetRequiredService<MainWindowViewModel>();
+       var membershipDataGridViewModel = App.Services.GetRequiredService<MembershipDataGridViewModel>();
+       var membershipDataGridView = new MembershipDataGrid
+       {
+           DataContext = membershipDataGridViewModel
+       };
+       
+       mainWindowViewModel.CurrentView = membershipDataGridView;
     }
 
 }
