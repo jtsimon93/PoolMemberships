e9c761d2adefd50c50bf1d668345832de280c21f
Justin Simon <jtsimon@jtsimon.dev>
2024-07-28 12:55:05 -0400

    Add update DTOs

---
+++ PoolMemberships/Dtos/UpdateMembershipDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoolMemberships.Dtos;
+
+public class UpdateMembershipDto
+{
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public string? KeyFobId { get; set; }
+    public bool Active { get; set; } = true;
+    public string? Notes { get; set; }
+}
